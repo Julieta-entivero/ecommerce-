10654ffe225ffc7f6349fb9021d772e451271457
Julieta Belen Entivero <julietabentivero@gmail.com>
2026-03-17 21:15:00 -0300

    page object de productos y tests de ordenamiento

---
+++ src/main/java/com/shopzone/pages/ProductsPage.java
@@ -0,0 +1,107 @@
+package com.shopzone.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductsPage extends BasePage {
+
+    @FindBy(className = "title")
+    private WebElement pageTitle;
+
+    @FindBy(className = "product_sort_container")
+    private WebElement sortDropdown;
+
+    @FindBy(className = "inventory_item")
+    private List<WebElement> productItems;
+
+    @FindBy(className = "shopping_cart_link")
+    private WebElement cartLink;
+
+    @FindBy(className = "shopping_cart_badge")
+    private WebElement cartBadge;
+
+    public ProductsPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public boolean isPageLoaded() {
+        try {
+            return getText(pageTitle).equals("Products");
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public int getProductCount() {
+        return productItems.size();
+    }
+
+    public void addProductToCart(int index) {
+        WebElement product = productItems.get(index);
+        WebElement addBtn = product.findElement(By.cssSelector("button[id^='add-to-cart']"));
+        click(addBtn);
+    }
+
+    public void addProductByName(String productName) {
+        for (WebElement item : productItems) {
+            String name = item.findElement(By.className("inventory_item_name")).getText();
+            if (name.equals(productName)) {
+                WebElement addBtn = item.findElement(By.cssSelector("button[id^='add-to-cart']"));
+                click(addBtn);
+                return;
+            }
+        }
+    }
+
+    public void removeProductByName(String productName) {
+        for (WebElement item : productItems) {
+            String name = item.findElement(By.className("inventory_item_name")).getText();
+            if (name.equals(productName)) {
+                WebElement removeBtn = item.findElement(By.cssSelector("button[id^='remove']"));
+                click(removeBtn);
+                return;
+            }
+        }
+    }
+
+    public String getCartBadgeCount() {
+        try {
+            return cartBadge.getText();
+        } catch (Exception e) {
+            return "0";
+        }
+    }
+
+    public CartPage goToCart() {
+        click(cartLink);
+        return new CartPage(driver);
+    }
+
+    public void sortBy(String option) {
+        Select select = new Select(sortDropdown);
+        select.selectByVisibleText(option);
+    }
+
+    public List<String> getProductNames() {
+        List<String> names = new ArrayList<>();
+        for (WebElement item : productItems) {
+            names.add(item.findElement(By.className("inventory_item_name")).getText());
+        }
+        return names;
+    }
+
+    public List<Double> getProductPrices() {
+        List<Double> prices = new ArrayList<>();
+        for (WebElement item : productItems) {
+            String priceText = item.findElement(By.className("inventory_item_price")).getText();
+            prices.add(Double.parseDouble(priceText.replace("$", "")));
+        }
+        return prices;
+    }
+}
---
+++ src/test/java/com/shopzone/tests/ProductsTest.java
@@ -0,0 +1,82 @@
+package com.shopzone.tests;
+
+import com.shopzone.base.BaseTest;
+import com.shopzone.pages.LoginPage;
+import com.shopzone.pages.ProductsPage;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class ProductsTest extends BaseTest {
+
+    private ProductsPage productsPage;
+
+    @BeforeMethod
+    public void loginAndInit() {
+        LoginPage loginPage = new LoginPage(driver);
+        productsPage = loginPage.login("standard_user", "secret_sauce");
+    }
+
+    @Test
+    public void testProductosSeCargan() {
+        Assert.assertTrue(productsPage.isPageLoaded());
+        Assert.assertEquals(productsPage.getProductCount(), 6);
+    }
+
+    @Test
+    public void testAgregarProductoAlCarrito() {
+        productsPage.addProductByName("Sauce Labs Backpack");
+        Assert.assertEquals(productsPage.getCartBadgeCount(), "1");
+    }
+
+    @Test
+    public void testAgregarVariosProductos() {
+        productsPage.addProductByName("Sauce Labs Backpack");
+        productsPage.addProductByName("Sauce Labs Bike Light");
+        Assert.assertEquals(productsPage.getCartBadgeCount(), "2");
+    }
+
+    @Test
+    public void testRemoverProductoDelCarrito() {
+        productsPage.addProductByName("Sauce Labs Backpack");
+        Assert.assertEquals(productsPage.getCartBadgeCount(), "1");
+
+        productsPage.removeProductByName("Sauce Labs Backpack");
+        Assert.assertEquals(productsPage.getCartBadgeCount(), "0");
+    }
+
+    @Test
+    public void testOrdenarPorPrecioMenorAMayor() {
+        productsPage.sortBy("Price (low to high)");
+        List<Double> prices = productsPage.getProductPrices();
+
+        for (int i = 0; i < prices.size() - 1; i++) {
+            Assert.assertTrue(prices.get(i) <= prices.get(i + 1),
+                "Los precios no estan ordenados de menor a mayor");
+        }
+    }
+
+    @Test
+    public void testOrdenarPorPrecioMayorAMenor() {
+        productsPage.sortBy("Price (high to low)");
+        List<Double> prices = productsPage.getProductPrices();
+
+        for (int i = 0; i < prices.size() - 1; i++) {
+            Assert.assertTrue(prices.get(i) >= prices.get(i + 1),
+                "Los precios no estan ordenados de mayor a menor");
+        }
+    }
+
+    @Test
+    public void testOrdenarPorNombreAZ() {
+        productsPage.sortBy("Name (A to Z)");
+        List<String> names = productsPage.getProductNames();
+
+        for (int i = 0; i < names.size() - 1; i++) {
+            Assert.assertTrue(names.get(i).compareTo(names.get(i + 1)) <= 0,
+                "Los nombres no estan en orden A-Z");
+        }
+    }
+}
